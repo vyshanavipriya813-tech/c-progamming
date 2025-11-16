7ef852ec6364977d884e7251253cb6cf31d3d143
vyshanavipriya813-tech <vyshanavipriya813@gmail.com>
2025-11-16 15:01:34 +0530

    addition.c

---
+++ addition.c
@@ -0,0 +1,25 @@
+int rows, columns;
+printf("Enter the number of rows: ");
+scanf("%d", Brows ) ;
+printf ("Enter the member of columns: ");
+scanf("xd", &columns);
+printf("\nEnter elements of Matrix A: \n");
+for (int i = 0; i < roms; i++) {
+for (int ] - @; ] < columns; j++) {
+printf("Enter element A[&d][&d]: ", i, i);
+scanf("%d", AmatrixA[i ][j]);
+printf("\nEnter elements of Matrix B;\n");
+for (int i - @; i < roms; i++) {
+for (int j - es j < columnsy j++) {
+printf ("Enter elevent B[%d][%d]: ", i, 1);
+scanf("%d", &matrix#[i ][j]);
+for (int 1 - 8] i < rows) i++) [
+for (int j - 0; i < columns; 1++) {
+resultMatrix[i][j] - matrix"[i][i] + matrixt[i][i];
+printf ("\nResultant Matrix (A + B):\n"}]
+for (int i - 0; i < rows; i++) {
+for(int j = 0; i < columns; i++) {
+printf("MNt", resultMitrix[ i] []]>;
+printf("\n");
+return 0;
+}
