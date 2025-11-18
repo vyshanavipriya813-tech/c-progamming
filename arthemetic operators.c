936a043911c495d2ca66063ab1cdfcd84a84e40e
vyshanavipriya813-tech <vyshanavipriya813@gmail.com>
2025-11-18 10:22:27 +0530

    arthemetic operators.c

---
+++ arthemetic operators.c
@@ -0,0 +1,13 @@
+include<stdio. h>
+int main()
+{
+int a,b;
+printf("enter two numbers:");
+scanf("%d%d", &a, &b);
+printf("%d+%d=%d\n", a, b, a+b);
+printf("%d-%d=%d\n", a, b, a-b);
+printf("%d*%d=%d\n", a, b, a*b);
+printf("%d/%d=%d\n", a, b, a/b);
+printf("%d%%%d=%d\n", a, b, a%b);
+return 0;
+}
