a53511be61e3994a8cf0c279ee63943dac451491
vyshanavipriya813-tech <vyshanavipriya813@gmail.com>
2025-11-18 10:41:33 +0530

    display fibonacci series using for loop.c

---
+++ display fibonacci series using for loop.c
@@ -0,0 +1,13 @@
+#include<stdio. h>
+int main()
+{
+int n,i, f1, f2,f3;
+printf("enter a number: ");
+scanf("%d",&n);
+for(i=0,f1=0, f2=1;i<n;i++)
+printf("%d\t", f1);
+f3=f1+f2;
+f1=f2;
+f2=f3;
+return 0;
+}
