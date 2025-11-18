db3ca18a79ad7338f5dd3afc6d8525674bbb3749
vyshanavipriya813-tech <vyshanavipriya813@gmail.com>
2025-11-18 11:26:28 +0530

    factorial of a given number using for loop.c

---
+++ factorial of a given number using for loop.c
@@ -0,0 +1,11 @@
+#include<stdio. h>
+int main()
+{
+int n,i, fact=1;
+printf("enter a number:");
+scanf("%d",&n);
+for(i=1;i <= n;i++)
+fact=fact*i;factorial of a given number
+printf("fact=%d\n", fact);
+return 0;
+}
