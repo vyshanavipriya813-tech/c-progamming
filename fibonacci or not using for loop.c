c15755ceb0887721f58884421fa6c469e4e38abe
vyshanavipriya813-tech <vyshanavipriya813@gmail.com>
2025-11-18 13:30:42 +0530

    fibonacci or not using for loop.c

---
+++ fibonacci or not using for loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n, f1, f2, f3;
+printf("enter a number :");
+scanf("%d" ,&n) ;
+for(f1=0,f2=1;n>=f1;f3=f1+f2,f1=f2,f2=f3)
+if(n == f1)
+{
+printf("YES");
+return 0;
+printf("NO");
+return 0;
+}
