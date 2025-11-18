e3ce5265b20df76616f399524142863995eb73e1
vyshanavipriya813-tech <vyshanavipriya813@gmail.com>
2025-11-18 10:29:04 +0530

    celsius to farenheit.c

---
+++ celsius to farenheit.c
@@ -0,0 +1,10 @@
+#include<stdio. h>
+int main()
+{
+float f,c;
+printf("enter the temperature in celsius:");
+scanf("%f",&c);
+f=9*c/5+32;
+printf("temperature in farenheit: %.2f\n",f);
+return 0;
+}
