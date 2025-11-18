174889211f27d7034e6fbc84573b601b0bcc5c35
vyshanavipriya813-tech <vyshanavipriya813@gmail.com>
2025-11-18 10:14:19 +0530

    area and perimeter of a circle.c

---
+++ area and perimeter of a circle.c
@@ -0,0 +1,13 @@
+#include<stdio. h>
+int main()
+{
+int r;
+float area, perimeter;
+printf("enter radius : ");
+scanf("%d",&r);
+area=3.14*r*r;
+perimeter=2*3.14*r;
+printf("area is %2f\n", area);
+printf("perimeter is %2f\n", perimeter);
+return 0;
+}
