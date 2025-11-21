70f435a653e3108e1776e67591e52a023de66e3d
vyshanavipriya813-tech <vyshanavipriya813@gmail.com>
2025-11-21 12:34:57 +0530

    if else if print a digit.c

---
+++ if else if print a digit.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter a number:");
+	scanf("%d",&n);
+	if(n==0)
+	printf("ZERO\n");
+	else if(n==1)
+	printf("ONE\n");
+	else if(n==2)
+	printf("TWO\n");
+	else if(n==3)
+	printf("THREE\n");
+	else if(n==4)
+	printf("FOUR\n");
+	else if(n==5)
+	printf("FIVE\n");
+	else if(n==6)
+	printf("SIX\n");
+	else if(n==7)
+	printf("SEVEN\n");
+	else if(n==8)
+	printf("EIGHT\n");
+	else if(n==9)
+	printf("NINE\n");
+	else
+	printf("INVALID DIGIT\n");
+	return 0;
+}
