2c613b5e21b25120d2532c45811f90faf2616a0a
vyshanavipriya813-tech <vyshanavipriya813@gmail.com>
2025-11-21 15:12:36 +0530

    simple if .c

---
+++ simple if .c
@@ -0,0 +1,13 @@
+//write a c program to find whether the given number is greater than 100 
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter a number:");
+	scanf("%d",&a);
+	if(a>100)
+	{
+	printf("a is greater than 100");
+    }
+	return 0;
+}
