a46c59fb30a1533941d0d3371fc32b96f245df1a
vyshanavipriya813-tech <vyshanavipriya813@gmail.com>
2025-11-21 11:51:50 +0530

    power(x,n).c

---
+++ power(x,n).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int b,e;
+	printf("enter base:");
+	scanf("%d",&b);
+	printf("enter base:");
+	scanf("%d",&b);
+	printf("enter exponent:");
+	scanf("%d",&e);
+	printf("pow(%d,%d)=%f\n",b,e,pow(b,e));
+	return 0;
+}
