d16d4efddc7223472581eff4f5468d58e80c85f1
vyshanavipriya813-tech <vyshanavipriya813@gmail.com>
2025-11-21 12:53:13 +0530

    palindrome using for loop.c

---
+++ palindrome using for loop.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int rev=0,n,r,m;
+	printf("enter a number:");
+	scanf("%d",&n);
+	m=n;
+	for(;n!=0;)
+	{
+		r=n%10;
+		rev=rev*10+r;
+		n=n/10;
+	}
+	if(rev==m)
+	{
+		printf("palindrome\n");
+	}
+	else
+	printf("not a palindrome\n");
+	return 0;
+}
