dd94c23b04c9eef4f946fe99035218ff6bc26c87
vyshanavipriya813-tech <vyshanavipriya813@gmail.com>
2025-11-26 20:45:46 +0530

    Fix formatting issues in README.c

---
+++ Fix formatting issues in README.c
@@ -0,0 +1,23 @@
+{
+case '+':printf("%d+%d=%d\n",a,b,a+b);
+break;
+	  case '-':printf("%d-%d=%d\n",a,b,a-b);
+	    case '-':printf("%d-%d=%d\n",a,b,a-b);
+break;
+case '*':printf("%d*%d=%d\n",a,b,a*b);
+break;
+case '/':if(b!=0)
+printf("%d/%d=%d\n",a,b,a/b);
+             else
+                 else
+printf("division by zero is not possible\n");
+break;
+case '%':if(b!=0)
+printf("%d%%%d=%d\n",a,b,a%b);
+else
+printf("modular division by zero is not possible\n");
+	  default:printf("invalid operation\n");
+	     default:printf("invalid operation\n");
+}
+return 0;
+}
