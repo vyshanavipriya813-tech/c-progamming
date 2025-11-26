94da27c175f581826b18f10ff1aae8001bf7f83f
vyshanavipriya813-tech <vyshanavipriya813@gmail.com>
2025-11-26 20:53:03 +0530

    Add program to calculate circle area and perimeter Calculate area and perimeter of a circle based on user input.c

---
+++ Add program to calculate circle area and perimeter Calculate area and perimeter of a circle based on user input.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int r;
+float area,perimeter;
+scanf("%d",&r);
+area=3.14*r*r;
+perimeter=2*3.14*r;
+printf("%.2f, area);
+printf("%.2f, perimeter);
+return 0;
+}
