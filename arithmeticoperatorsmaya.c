7b1e6c8baa8d8e823c6f205672e280484e57b7e8
vyshanavipriya813-tech <vyshanavipriya813@gmail.com>
2025-11-26 21:11:56 +0530

    arithmeticoperatorsmaya.c

---
+++ arithmeticoperatorsmaya.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int num1,num2;
+scanf("%d%d",&num1,&num2);
+int sum=num1+num2;
+int difference=num1-num2;
+int product=num1*num2;
+int quotient=num1/num2;
+int remainder=num1%num2;
+printf("Sum:%d, sum);
+printf("Difference : %d,difference);
+printf("Product : %d,product) ;
+printf("Quotient : %d,quotient);
+printf("Remainder : %d,remainder);
+return 0;
+  }
