4d0a975b7206244c411887ba06c741baad99e253
vyshanavipriya813-tech <vyshanavipriya813@gmail.com>
2025-11-27 10:30:29 +0530

    averageoftwonumbersmaya.c

---
+++ averageoftwonumbersmaya.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int num1,num2;
+float Average;
+scanf("%d",&num1);
+scanf("%d",&num2);
+Average=(float)(num1+num2)/2.0;
+printf("Average of %d and %d is :%.2f,num1, num2,Average) ;
+return 0;
+}
